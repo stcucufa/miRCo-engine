542658a74ae3ec7ecb964b6d03549cc26e94c37a
Claire Froelich <claire.froelich@gmail.com>
2025-05-18 09:59:25 -0400

    run prettier

--- games/wind-me-up/index.js
+++ games/wind-me-up/index.js
@@ -1,12 +1,12 @@
 export default class MircoGame {
   constructor({ input, assets, libs }) {
-    this.input = input;
-    this.assets = assets;
-    this.libs = libs;
+    this.input = input
+    this.assets = assets
+    this.libs = libs
     this.state = {
       gameOver: false,
       won: true,
-    };
+    }
   }
 
   init(canvas) {
@@ -27,182 +27,215 @@ export default class MircoGame {
       currentSeconds: 0,
       currentMinutes: 30,
       currentHours: 3,
-      currentSecondsOffset: 0, 
+      currentSecondsOffset: 0,
       currentMinutesOffset: 0,
-      currentHoursOffset:0,
+      currentHoursOffset: 0,
       expectedSeconds: Math.floor(Math.random() * 60),
       expectedMinutes: 30,
       expectedHours: 3,
 
-
       startTime: performance.now(),
 
-      message: "",
-    };
+      message: '',
+    }
 
-    this.state = { ...this.state, ...customState };
-    return;
+    this.state = { ...this.state, ...customState }
+    return
   }
 
   update(dt) {
-    const s = this.state;
-    if (s.gameOver) return;
+    const s = this.state
+    if (s.gameOver) return
 
     // Move player
     // if (this.input.isPressedLeft()) s.player.x -= 0.2 * dt;
     // if (this.input.isPressedRight()) s.player.x += 0.2 * dt;
 
-    // Move time 
+    // Move time
     // forwards
     if (this.input.isPressedUp()) {
-      s.currentMinutesOffset += 1;
+      s.currentMinutesOffset += 1
       if (s.currentMinutesOffset >= 60) {
-          s.currentMinutesOffset = 0;
-          s.currentHoursOffset += 1;
-          if (s.currentHoursOffset >= 24) {
-            s.currentHoursOffset = 0;
-          }
+        s.currentMinutesOffset = 0
+        s.currentHoursOffset += 1
+        if (s.currentHoursOffset >= 24) {
+          s.currentHoursOffset = 0
+        }
       }
-      if (s.currentHours === s.expectedHours && s.currentMinutes === s.expectedMinutes && s.currentSeconds === s.expectedSeconds) {
-        s.won = true;
+      if (
+        s.currentHours === s.expectedHours &&
+        s.currentMinutes === s.expectedMinutes &&
+        s.currentSeconds === s.expectedSeconds
+      ) {
+        s.won = true
         this.won()
       }
     }
     // backwards
     if (this.input.isPressedDown()) {
-      s.currentMinutesOffset -= 1;
+      s.currentMinutesOffset -= 1
       if (s.currentMinutesOffset < 0) {
-          s.currentMinutesOffset = 59;
-          s.currentHoursOffset -= 1;
-          if (s.currentHoursOffset < 0) {
-              s.currentHoursOffset = 23;
-          }
+        s.currentMinutesOffset = 59
+        s.currentHoursOffset -= 1
+        if (s.currentHoursOffset < 0) {
+          s.currentHoursOffset = 23
+        }
       }
-      if (s.currentHours === s.expectedHours && s.currentMinutes === s.expectedMinutes && s.currentSeconds === s.expectedSeconds) {
-        s.won = true;
+      if (
+        s.currentHours === s.expectedHours &&
+        s.currentMinutes === s.expectedMinutes &&
+        s.currentSeconds === s.expectedSeconds
+      ) {
+        s.won = true
         this.won()
       }
     }
-    this.draw();
+    this.draw()
   }
 
   draw() {
-    const s = this.state;
-    const p5 = this.libs.p5;
-    const cx = 400;
-    const cy = 300;
-    p5.background(255);
+    const s = this.state
+    const p5 = this.libs.p5
+    const cx = 400
+    const cy = 300
+    p5.background(255)
     // p5.translate(200, 150);
 
-    const innerRadius = 100;
-    const secondaryInnerRadius = 110;
-    const outerRadius = 130;
-    const numTicks = 12; // Number of ticks around the circle
+    const innerRadius = 100
+    const secondaryInnerRadius = 110
+    const outerRadius = 130
+    const numTicks = 12 // Number of ticks around the circle
 
-    let seconds = p5.map(s.currentSeconds + s.currentSecondsOffset, 0, 60, 0, p5.TWO_PI) - p5.HALF_PI;
-    const secondsRadius = 80;
+    let seconds =
+      p5.map(s.currentSeconds + s.currentSecondsOffset, 0, 60, 0, p5.TWO_PI) -
+      p5.HALF_PI
+    const secondsRadius = 80
 
-    let minutes = p5.map(s.currentMinutes + s.currentMinutesOffset + p5.norm(s.currentSeconds + s.currentSecondsOffset, 0, 60), 0, 60, 0, p5.TWO_PI) - p5.HALF_PI;
-    const minutesRadius = 80;
+    let minutes =
+      p5.map(
+        s.currentMinutes +
+          s.currentMinutesOffset +
+          p5.norm(s.currentSeconds + s.currentSecondsOffset, 0, 60),
+        0,
+        60,
+        0,
+        p5.TWO_PI
+      ) - p5.HALF_PI
+    const minutesRadius = 80
 
-    let hours = p5.map(s.currentHours + s.currentHoursOffset + p5.norm(s.currentMinutes + s.currentMinutesOffset, 0, 60), 0, 24, 0, p5.TWO_PI * 2) - p5.HALF_PI;
-    const hoursRadius = 80;
+    let hours =
+      p5.map(
+        s.currentHours +
+          s.currentHoursOffset +
+          p5.norm(s.currentMinutes + s.currentMinutesOffset, 0, 60),
+        0,
+        24,
+        0,
+        p5.TWO_PI * 2
+      ) - p5.HALF_PI
+    const hoursRadius = 80
 
-    // draw milanese band 
+    // draw milanese band
     let doFill = true
     let milaneseWidth = 8
     let milaneseHeight = 16
     let colCount = 16
-    let borderRadius = 2;
-    let rowCount = 100;
-    let milaneseShade = 160;
-    p5.push(); // black stroke
-    p5.fill(milaneseShade);
-    p5.stroke(milaneseShade);
-    p5.strokeWeight(2);
+    let borderRadius = 2
+    let rowCount = 100
+    let milaneseShade = 160
+    p5.push() // black stroke
+    p5.fill(milaneseShade)
+    p5.stroke(milaneseShade)
+    p5.strokeWeight(2)
     // rows
     for (let x = 0; x <= 100; x++) {
       // columns
       for (let y = 0; y <= colCount; y++) {
-        if ((y + x % 2) % 2 === 0) {
-          p5.rect((cx - milaneseWidth * colCount/2) + y * milaneseWidth,x * milaneseHeight,milaneseWidth,milaneseHeight, borderRadius, borderRadius, borderRadius, borderRadius)
-
-        } 
+        if ((y + (x % 2)) % 2 === 0) {
+          p5.rect(
+            cx - (milaneseWidth * colCount) / 2 + y * milaneseWidth,
+            x * milaneseHeight,
+            milaneseWidth,
+            milaneseHeight,
+            borderRadius,
+            borderRadius,
+            borderRadius,
+            borderRadius
+          )
+        }
       }
     }
-    p5.pop();
+    p5.pop()
 
-      // winder
-    let winderFill = 160;
-    p5.push();
-    p5.translate(cx + outerRadius - 10, cy);
-    p5.stroke(winderFill);
-    p5.strokeWeight(2);
-    p5.fill(winderFill);
-    p5.rect(0,-25,25,50)
-    p5.pop();
-    
-      // Draw outer circle
-    let outerCircleFill = 40;
-    p5.push();
-    p5.translate(cx, cy);
-    p5.stroke(outerCircleFill);
-    p5.strokeWeight(2);
-    p5.fill(outerCircleFill);
-    p5.ellipse(0, 0, outerRadius * 2);
-    p5.pop();
+    // winder
+    let winderFill = 160
+    p5.push()
+    p5.translate(cx + outerRadius - 10, cy)
+    p5.stroke(winderFill)
+    p5.strokeWeight(2)
+    p5.fill(winderFill)
+    p5.rect(0, -25, 25, 50)
+    p5.pop()
 
+    // Draw outer circle
+    let outerCircleFill = 40
+    p5.push()
+    p5.translate(cx, cy)
+    p5.stroke(outerCircleFill)
+    p5.strokeWeight(2)
+    p5.fill(outerCircleFill)
+    p5.ellipse(0, 0, outerRadius * 2)
+    p5.pop()
 
-    // Draw secondary inner circle 
-    let secCircleFill = 160;
-    p5.push();
-    p5.translate(cx, cy);
-    p5.stroke(secCircleFill);
-    p5.strokeWeight(2);
-    p5.fill(secCircleFill);
-    p5.ellipse(0, 0, secondaryInnerRadius * 2);
-    p5.pop();
+    // Draw secondary inner circle
+    let secCircleFill = 160
+    p5.push()
+    p5.translate(cx, cy)
+    p5.stroke(secCircleFill)
+    p5.strokeWeight(2)
+    p5.fill(secCircleFill)
+    p5.ellipse(0, 0, secondaryInnerRadius * 2)
+    p5.pop()
 
-        // Draw inner circle
-    let innerCircleFill = 100;
-    p5.push();
-    p5.translate(cx, cy);
-    p5.stroke(innerCircleFill);
-    p5.strokeWeight(2);
-    p5.fill(innerCircleFill);
-    p5.ellipse(0, 0, innerRadius * 2);
-    p5.pop();
+    // Draw inner circle
+    let innerCircleFill = 100
+    p5.push()
+    p5.translate(cx, cy)
+    p5.stroke(innerCircleFill)
+    p5.strokeWeight(2)
+    p5.fill(innerCircleFill)
+    p5.ellipse(0, 0, innerRadius * 2)
+    p5.pop()
 
-
-        // draw-hands
-    p5.strokeWeight(2);
-    p5.stroke(0); 
+    // draw-hands
+    p5.strokeWeight(2)
+    p5.stroke(0)
 
     // // draw seconds-hand
-    p5.push();
-    p5.translate(cx, cy);
-    p5.rotate(seconds);
-    p5.line(0, 0, 0, secondsRadius);
-    p5.pop();
+    p5.push()
+    p5.translate(cx, cy)
+    p5.rotate(seconds)
+    p5.line(0, 0, 0, secondsRadius)
+    p5.pop()
 
     // // Minute hand
-    p5.push();
-    p5.translate(cx, cy);
-    p5.rotate(minutes);
-    p5.strokeWeight(3);
-    p5.line(0, 0, 0, minutesRadius);
-    p5.pop();
+    p5.push()
+    p5.translate(cx, cy)
+    p5.rotate(minutes)
+    p5.strokeWeight(3)
+    p5.line(0, 0, 0, minutesRadius)
+    p5.pop()
 
     // // Hour hand
-    p5.push();
-    p5.translate(cx, cy);
-    p5.rotate(hours);
-    p5.strokeWeight(4);
-    p5.line(0, 0, 0, hoursRadius);
-    p5.pop();
+    p5.push()
+    p5.translate(cx, cy)
+    p5.rotate(hours)
+    p5.strokeWeight(4)
+    p5.line(0, 0, 0, hoursRadius)
+    p5.pop()
 
     // draw sawtooth outline
-    // let secondaryOuterRadius = 135;    
+    // let secondaryOuterRadius = 135;
     // let numTeeth = 30;
     // let toothHeight = 10;
     // p5.beginShape();
@@ -227,7 +260,6 @@ export default class MircoGame {
     // p5.endShape(p5.CLOSE);
     // END: sawtooth circle
 
-
     // Draw the ticks
     // p5.strokeWeight(1);
     // for (let i = 0; i < numTicks; i++) {
@@ -246,10 +278,10 @@ export default class MircoGame {
       a.x + a.width > b.x &&
       a.y < b.y + b.height &&
       a.y + a.height > b.y
-    );
+    )
   }
 
   end() {
-    return this.state.won;
+    return this.state.won
   }
 }
